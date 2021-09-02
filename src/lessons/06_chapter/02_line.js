8e51bf03d908fed7d44c561d9fe6a48d4e446011
Vishal Gautam <vishal.gautam@sofdesk.com>
2021-09-01 23:21:03 -0400

    Made it work

--- src/lessons/06_chapter/02_line.js
+++ src/lessons/06_chapter/02_line.js
@@ -1,3 +1,137 @@
+const matrix4 = (() => {
+  // scaling function
+  const fromScaling = v => {
+    // prettier-ignore
+    return new Float32Array([
+         v[0], 0,    0,    0, // x = 1st item  - 0th index
+         0,    v[1], 0,    0, // y = 6th item  - 5th index
+         0,    0,    v[2], 0, // z = 11th item - 10th index
+         0,    0,    0,    1
+    ])
+  };
+
+  // Perform axis-specific matrix multiplication
+  const fromXRotation = rad => {
+    // prettier-ignore
+    return new Float32Array([
+      1,  0,             0,             0,
+      0,  Math.cos(rad), Math.sin(rad), 0,
+      0, -Math.sin(rad), Math.cos(rad), 0,
+      0, 0,              0,             1
+    ])
+  };
+  const fromYRotation = rad => {
+    // prettier-ignore
+    return new Float32Array([
+      Math.cos(rad), 0, -Math.sin(rad), 0,
+      0,             1,  0,             0,
+      Math.sin(rad), 0,  Math.cos(rad), 0,
+      0,             0,  0,             1
+    ])
+  };
+  const fromZRotation = rad => {
+    // prettier-ignore
+    return new Float32Array([
+       Math.cos(rad), Math.sin(rad), 0,  0,
+      -Math.sin(rad), Math.cos(rad), 0,  0,
+       0,             0,             1,  0,
+       0,             0,             0,  1
+    ])
+  };
+  // Matrix helpers
+  // translate in x or y axis
+  const translate = ([x, y, z]) => {
+    // prettier-ignore
+    return new Float32Array([
+      1, 0, 0, 0,
+      0, 1, 0, 0,
+      0, 0, 1, 0,
+      x, y, z, 1
+    ])
+  };
+
+  return {
+    fromScaling,
+    fromXRotation,
+    fromYRotation,
+    fromZRotation,
+    translate
+  };
+})();
+
+const updateTransformationMatrix = (
+  modelMat,
+  translation,
+  rotation,
+  scaling,
+  props
+) => {
+  const {
+    math: { mat4 }
+  } = props;
+  window.mat4 = mat4;
+
+  rotation = rotation || [2, 0, 0];
+  translation = translation || [0, 0, 0];
+  scaling = scaling || [2, 2, 1];
+
+  //perform indivisual transformations
+
+  const translateMat = matrix4.translate(translation);
+  window.translateMat = translateMat;
+  `[1   0   0  0
+    0   1   0  0
+    0   0   1  0
+    0, -5 -10  1]
+    ]
+  `;
+
+  const rotateXMat = matrix4.fromXRotation(rotation[0]);
+  const rotateYMat = matrix4.fromYRotation(rotation[1]);
+  const rotateZMat = matrix4.fromZRotation(rotation[2]);
+  const scaleMat = matrix4.fromScaling(scaling);
+
+  //combine all transformation matrices together to form a final transform matrix: modelMat
+  window.modelA = modelMat.slice();
+  // == console.log(window.modelA);
+  `[1, 0, 0, 0,
+    0, 1, 0, 0,
+    0, 0, 1, 0,
+    0, 0, 0, 1,]
+  `;
+
+  mat4.multiply(modelMat, rotateXMat, scaleMat);
+  window.modelX = modelMat.slice();
+  // == console.log(window.modelX);
+  `[ 2, 0, 0, 0,
+     0, 2, 0, 0,
+     0, 0, 1, 0,
+     0, 0, 0, 1]
+  `;
+  mat4.multiply(modelMat, rotateYMat, modelMat);
+  window.modelY = modelMat.slice();
+
+  `[ 2.1612091064453125, 0, 3.3658838272094727, 0,
+     0,                  2, 0,                  0,
+    -2.5244128704071045, 0, 1.6209068298339844, 0,
+     0,                  0, 0,                  1,`;
+
+  mat4.multiply(modelMat, rotateZMat, modelMat);
+  window.modelZ = modelMat.slice();
+  `[ 2.1612091064453125, 0, 3.3658838272094727, 0,
+     0,                  2, 0,                  0,
+    -2.5244128704071045, 0, 1.6209068298339844, 0,
+     0,                  0, 0,                  1,]
+  `;
+
+  mat4.multiply(modelMat, translateMat, modelMat);
+  window.result = modelMat.slice();
+  `[ 2.1612091064453125,  0,   3.3658838272094727, 0,
+     0,                   2,   0,                  0,
+    -2.5244128704071045,  0,   1.6209068298339844, 0,
+     0,                  -5, -10,                  1,]`;
+};
+
 const gpuInit = async ({ canvas }) => {
   // Create adapter, device and WebGPU context
   const adapter = await navigator.gpu.requestAdapter();
@@ -106,8 +240,8 @@ const createViewProjection = ({ isPerspective, aspectRatio }, props) => {
   };
 
   return {
-    // viewMatrix,
-    // projectionMatrix,
+    viewMatrix,
+    projectionMatrix,
     viewProjectionMatrix,
     cameraOption
   };
@@ -120,14 +254,17 @@ function createLine3DData() {
     let x = Math.exp(-t) * Math.sin(30 * t);
     let z = Math.exp(-t) * Math.cos(30 * t);
     let y = 2 * t - 1;
+    // pushing 3 items per loop, total of 900 items
     data.push(x, y, z);
   }
   return data.flat(1);
 }
 
+window.createLine3DData = createLine3DData;
+
 export async function createLine(props) {
   const {
-    math: { mat4 },
+    math: { mat4, vec3 },
     canvas
   } = props;
   // console.log("PROPS", props);
@@ -135,20 +272,207 @@ export async function createLine(props) {
     canvas
   });
 
+  // window.swapChainFormat = swapChainFormat;
+
+  // window.props = props;
+
   // Create Vertices
   const lineData = createLine3DData();
-  const vertexBuffer = createGPUBuffer(device, lineData);
+  const data = new Float32Array(lineData);
+  window.data = data;
+  const vertexBuffer = device.createBuffer({
+    size: data.byteLength,
+    usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
+    mappedAtCreation: true
+  });
+
+  // 3: Get Access to the Content
+  new Float32Array(vertexBuffer.getMappedRange()).set(data);
+  // 4. Unmap so that data can be used by the GPU
+  vertexBuffer.unmap();
 
   //create uniform data
-  const modelMatrix = mat4.create();
-  const mvpMatrix = mat4.create();
-  let vMatrix = mat4.create();
-  const viewProjection = createViewProjection(
-    { aspectRatio: canvas.width / canvas.height, isPerspective: true },
-    props
-  );
+  // const modelMatrix = mat4.create();
+  // const mvpMatrix = mat4.create();
+  // let vMatrix = mat4.create();
+  // let vpMatrix = mat4.create();
+  // const viewProjection = createViewProjection(
+  //   { aspectRatio: canvas.width / canvas.height, isPerspective: true },
+  //   props
+  // );
 
-  let vpMatrix = viewProjection.viewProjectionMatrix;
+  // vpMatrix = viewProjection.viewProjectionMatrix;
+  // const camera = props.camera(canvas, viewProjection.cameraOption);
 
-  console.log("LINE", vpMatrix);
+  // window.camera = camera;
+
+  `Step 3: CREATE Render Pipeline`;
+  const shaders = {
+    vertex: `
+          // [[block]] struct Uniforms {
+          //     [[offset(0)]] mvpMatrix : mat4x4<f32>;
+          // };
+          // [[binding(0), group(0)]] var<uniform> uniforms : Uniforms;
+
+          [[stage(vertex)]]
+          fn main([[location(0)]] pos: vec4<f32>) ->  [[builtin(position)]] vec4<f32> {
+              // return uniforms.mvpMatrix * pos;
+              return pos;
+          }`,
+
+    fragment: `
+          [[stage(fragment)]]
+          fn main() -> [[location(0)]] vec4<f32> {
+              return vec4<f32>(1.0, 1.0, 0.0, 1.0);
+          }`
+  };
+
+  const pipeline = device.createRenderPipeline({
+    vertex: {
+      module: device.createShaderModule({
+        code: shaders.vertex
+      }),
+      entryPoint: "main",
+      buffers: [
+        {
+          arrayStride: 12,
+          attributes: [
+            {
+              shaderLocation: 0,
+              format: "float32x3",
+              offset: 0
+            }
+          ]
+        }
+      ]
+    },
+    fragment: {
+      module: device.createShaderModule({
+        code: shaders.fragment
+      }),
+      entryPoint: "main",
+      targets: [{ format: swapChainFormat }]
+    },
+    primitive: {
+      topology: "line-strip",
+      stripIndexFormat: "uint32"
+    }
+  });
+
+  //create uniform buffer and bind group
+  // const sceneUniformBuffer = device.createBuffer({
+  //   size: 64,
+  //   usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST
+  // });
+
+  // const sceneUniformBindGroup = device.createBindGroup({
+  //   layout: pipeline.getBindGroupLayout(0),
+  //   entries: [
+  //     {
+  //       binding: 0,
+  //       resource: {
+  //         buffer: sceneUniformBuffer,
+  //         offset: 0,
+  //         size: 64
+  //       }
+  //     }
+  //   ]
+  // });
+
+  const app = {
+    loopID: null,
+    stopped: false
+  };
+
+  function run(f) {
+    const frame = t => {
+      f();
+
+      // for (const k in app.keyStates) {
+      //   app.keyStates[k] = processBtnState(app.keyStates[k]);
+      // }
+
+      // app.mouseState = processBtnState(app.mouseState);
+      // app.charInputted = [];
+      // app.mouseMoved = false;
+      app.loopID = requestAnimationFrame(frame);
+    };
+
+    app.stopped = false;
+    app.loopID = requestAnimationFrame(frame);
+  }
+
+  const createTransforms = (modelMat, translation, rotation, scaling) => {
+    const rotateXMat = mat4.create();
+    const rotateYMat = mat4.create();
+    const rotateZMat = mat4.create();
+    const translateMat = mat4.create();
+    const scaleMat = mat4.create();
+
+    rotation = rotation || [0, 0, 0];
+    translation = translation || [0, 0, 0];
+    scaling = scaling || [1, 1, 1];
+
+    //perform indivisual transformations
+    mat4.fromTranslation(translateMat, translation);
+    mat4.fromXRotation(rotateXMat, rotation[0]);
+    mat4.fromYRotation(rotateYMat, rotation[1]);
+    mat4.fromZRotation(rotateZMat, rotation[2]);
+    mat4.fromScaling(scaleMat, scaling);
+
+    //combine all transformation matrices together to form a final transform matrix: modelMat
+    mat4.multiply(modelMat, rotateXMat, scaleMat);
+    mat4.multiply(modelMat, rotateYMat, modelMat);
+    mat4.multiply(modelMat, rotateZMat, modelMat);
+    mat4.multiply(modelMat, translateMat, modelMat);
+  };
+
+  // const updateCamera = () => {};
+  const updateCamera = () => {
+    console.log("LOGGED");
+    createTransforms(
+      modelMatrix,
+      // translation
+      [0, 0, 0],
+      // rotation
+      [0, 0, 0],
+      // scaling
+      [1, 1, 1]
+    );
+
+    mat4.multiply(mvpMatrix, vpMatrix, modelMatrix);
+    device.queue.writeBuffer(sceneUniformBuffer, 0, mvpMatrix);
+  };
+
+  `Step 5: Draw`;
+  const draw = () => {
+    // update camera
+    // updateCamera();
+
+    // command encoder
+    const commandEncoder = device.createCommandEncoder();
+
+    // render pass
+    const renderPass = commandEncoder.beginRenderPass({
+      colorAttachments: [
+        {
+          view: context.getCurrentTexture().createView(),
+          loadValue: [0.5, 0.5, 1.0, 1.0],
+          storeOp: "store"
+        }
+      ]
+    });
+    renderPass.setPipeline(pipeline);
+    renderPass.setVertexBuffer(0, vertexBuffer);
+    // renderPass.setBindGroup(0, sceneUniformBindGroup);
+    renderPass.draw(300, 1, 0, 0);
+    renderPass.endPass();
+
+    // 8. submit
+    device.queue.submit([commandEncoder.finish()]);
+  };
+
+  draw();
+
+  // console.log(sceneUniformBindGroup);
 }
--- src/webgpu/core.cljs
+++ src/webgpu/core.cljs
@@ -11,7 +11,7 @@
    ; [webgpu.lessons.eight-buffer-one-triangle :as eight]))
    ; [webgpu.lessons.nine-transformation :as nine]))
    ; [webgpu.lessons.ten-projection :as ten]
-   ; [webgpu.lessons.ch06.cube-vertex-colors :as eleven]))
+   ; [webgpu.lessons.ch06.cube-vertex-colors :as eleven]
    [webgpu.lessons.ch06.line :as twelve]))
    ; [webgpu.lessons.genka.basic-scene-renderer :as bsr]))
    ;; Computing Examples
